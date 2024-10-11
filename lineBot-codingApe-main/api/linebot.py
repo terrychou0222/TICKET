5bb69746933a80b0ea3bcf63fd7d3fafaf9b9a69
bo306 <123080633+bo306@users.noreply.github.com>
2024-10-11 15:16:23 +0800

    Add files via upload

---
+++ lineBot-codingApe-main/api/linebot.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
+import os
+import html  # 使用標準庫的 escape 函數
+
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route("/webhook", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@line_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    escaped_message = html.escape(event.message.text)  # 使用 html.escape
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=escaped_message))
+
+if __name__ == "__main__":
+    app.run()
