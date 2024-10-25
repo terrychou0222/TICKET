b68ac1cd8514c3109d7489e8e1a28e77086781fc
terrychou0222 <140190983+terrychou0222@users.noreply.github.com>
2024-10-25 15:57:24 +0800

    Update linebot.py

--- api/linebot.py
+++ api/linebot.py
@@ -14,28 +14,6 @@ app = Flask(__name__)
 def home():
     return 'Hello, World!'
 
-@app.route("/webhook", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    try:
-        line_handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
-@line_handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    if event.message.text.lower() == "test":
-        reply_message = "第32行的reply_message=改成自己想傳送的訊息"
-    else:
-        reply_message = event.message.text
-    
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply_message))
 
 if __name__ == "__main__":
     app.run()
