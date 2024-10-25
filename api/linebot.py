b1fe84f2c8b24cee8573d070e158cc1c6905a3d3
terrychou0222 <140190983+terrychou0222@users.noreply.github.com>
2024-10-25 16:56:50 +0800

    Update linebot.py

--- api/linebot.py
+++ api/linebot.py
@@ -1,19 +1,10 @@
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
-import os
-import html  # 使用標準庫的 escape 函數
-
-line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
-line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
-
 
 if __name__ == "__main__":
     app.run()
